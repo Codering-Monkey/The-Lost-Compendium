9c35db54491916174f4ec5f3396cecabf461bfc6
MaxGa1ea <max.galea@sydstu.catholic.edu.au>
2025-10-24 10:55:26 +1100

    pycharm updates to index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 import data from "../data/index.json" with { type: 'json' }
-import { IdGet } from "../scripts/script.js"
+import { IdGet } from "./script"
 
 const tiles = IdGet("tiles")
 let available_info = data
