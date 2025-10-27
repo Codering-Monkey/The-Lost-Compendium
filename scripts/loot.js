d284c1a1df18baf16ce874000340cacb8ad2c610
MaxGa1ea <max.galea@sydstu.catholic.edu.au>
2025-10-27 14:33:51 +1100

    test

--- scripts/loot.js
+++ scripts/loot.js
@@ -76,7 +76,7 @@ function loadTiles() {
         const cr = parseFloat(monster[key]["CR"])
         for (let j = 0; j < value; j++) {
             monetaryValue += diceCalc(loot["Value"][cr][0], loot["Value"][cr][1], 0, loot["Value"][cr][2]) // money is calculated in cp
-        }
+        } //whack a doodle doo
         textplate.textContent = monetaryValue + " cp"
     })
 }
